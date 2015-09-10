09df69b3254e991c9006c17ad6837054bcf7c5d6
lukasz.harasimowicz <developer@hardev.pl>
2015-09-10 23:36:11 +0200

    Some performance improvements.
    Added caching.

--- web/apps/core/settings/base.py
+++ web/apps/core/settings/base.py
@@ -141,9 +141,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'drf_ujson.renderers.UJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'drf_ujson.parsers.UJSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
     ),
     'PAGE_SIZE': 10
 }
--- web/apps/wikia_auth/views.py
+++ web/apps/wikia_auth/views.py
@@ -5,7 +5,10 @@ from oauth2_provider.ext.rest_framework import TokenHasScope
 from oauth2_provider.models import AccessToken
 from rest_framework import generics
 from rest_framework import permissions
-
+from django.core import cache
+from rest_framework_extensions.cache.decorators import (
+    cache_response
+)
 from .serializers import AccessTokenSerializer
 
 
@@ -15,6 +18,10 @@ class TokenInfoView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope, ]
     required_scopes = ['read']
     lookup_field = 'token'
+
+    @cache_response()
+    def get(self, request, *args, **kwargs):
+        return super(TokenInfoView, self).get(request, *args, **kwargs)
 
     def get_object(self):
         self.kwargs[self.lookup_field] = self.request.auth.token
