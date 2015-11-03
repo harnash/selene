982ea366b42eaaaaa7c606284b2ce516870fddee
Łukasz Harasimowicz <dev@harnash.eu>
2015-11-03 12:06:40 +0100

    Added some initial documentation to the code.

--- web/apps/common/__init__.py
+++ web/apps/common/__init__.py
@@ -4,5 +4,16 @@ from __future__ import unicode_literals
 import random
 import string
 
+
 def rand_str(length, chars=string.ascii_uppercase + string.digits):
+    """
+    Utility function which generate random string from a given character list
+
+    Args:
+        length (int): length of the generated string
+        chars (list): list of characters to generate string from
+
+    Returns:
+        string: randomly generated string
+    """
     return ''.join(random.choice(chars) for _ in range(length))
